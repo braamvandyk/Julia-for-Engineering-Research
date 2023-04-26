e5c394eef4f93e428cc03bfa1101f17d1d86748a
Braam van Dyk <braam.vandyk@gmail.com>
2023-04-26 11:10:45 +0200

    Added optimisation

--- scrathpad.jl
+++ scrathpad.jl
@@ -1,38 +1,26 @@
-# Import the Base functions we are going to extend
-import Base: +, /, convert, promote_rule
+using Optimization, OptimizationOptimJL, OptimizationBBO, BenchmarkTools
 
-# Define our Dual number type as a sub-type of Number
-struct Dual <: Number
-    f::Tuple{Float64, Float64}
-end
+rosenbrock(u, p) = (p[1] - u[1])^2 + p[2] * (u[2] - u[1]^2)^2
+u0 = zeros(2)
+p = [1.0, 100.0]
 
-# Tell Julia how to add and divide with Dual
-+(x::Dual, y::Dual) = Dual(x.f .+ y.f)
-/(x::Dual, y::Dual) = Dual((x.f[1]/y.f[1], (y.f[1]*x.f[2] - x.f[1]*y.f[2])/y.f[1]^2))
+# Use NelderMead from Optim.jl, linked in via OptimizationOptimJL
 
-# Tell Julia how to deal with type conversions
-convert(::Type{Dual}, x::Real) = Dual((float(x), 0.0))
-promote_rule(::Type{Dual}, ::Type{<:Number}) = Dual
+prob = OptimizationProblem(rosenbrock, u0, p)
+@btime sol = solve(prob, NelderMead())
 
-# The Babylonian algorithm for square roots
-function babylonian(x, n = 10)
-    a = (x + 1)/2
-    for i in 2:n
-        a = (a + x/a)/2
-    end
-    return a
-end
+# 945.400 μs (3309 allocations: 168.98 KiB)
+# u: 2-element Vector{Float64}:
+#  0.9999634355313174
+#  0.9999315506115275
 
-# Test our algorithm
-babylonian(2)
-err = babylonian(2) - √2
+# Use Adaptive Differential Evolution method adaptive_de_rand_1_bin_radiuslimited from
+# BlackBoxOptim.jl, via OptimizationBBO. This method needs a bounded search area.
 
-# Create a dual number, with the epsilon part set to 1, to "seed" the derivative for the first variable
-d = Dual((2., 1.))
+prob = OptimizationProblem(rosenbrock, u0, p, lb = [-1.0, -1.0], ub = [1.0, 1.0])
+sol = solve(prob, BBO_adaptive_de_rand_1_bin_radiuslimited())
 
-# Run the same code we just used for real numbers
-res = babylonian(d)
-
-# Check the results
-res.f[1] - √2
-res.f[2] - 0.5/√2
+# 13.260 ms (153718 allocations: 5.69 MiB)
+# u: 2-element Vector{Float64}:
+#  0.9999999999999495
+#  0.9999999999998918
