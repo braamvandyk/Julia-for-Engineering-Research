7549be5f04ba968425fdc3317e8dc0810e04f8b8
Braam van Dyk <braam.vandyk@gmail.com>
2023-05-07 21:09:46 +0200

    Added ODE part

--- scrathpad.jl
+++ scrathpad.jl
@@ -1,26 +1,68 @@
-using Optimization, OptimizationOptimJL, OptimizationBBO, BenchmarkTools
+using DifferentialEquations, Plots
 
-rosenbrock(u, p) = (p[1] - u[1])^2 + p[2] * (u[2] - u[1]^2)^2
-u0 = zeros(2)
-p = [1.0, 100.0]
+"""
+The reactions rates for A + B -> C
+    u:  array with the molar flows of A, B and C
+    p:  tuple with P and T
+    t:  position in the reactor 0..L
+"""
+function rates!(du, u, p, t)
+    k₀ = 0.50
+    Eₐ = 100.0
+    T₀ = 500.0
+    R = 8.314
+    
+    # Convert molar flows to molar fractions
+    y = u ./ sum(u)
 
-# Use NelderMead from Optim.jl, linked in via OptimizationOptimJL
+    # Unpack the tuple to get pressure and temperature
+    P, ΔP, T = p
 
-prob = OptimizationProblem(rosenbrock, u0, p)
-@btime sol = solve(prob, NelderMead())
+    k = k₀ * exp(-Eₐ/R * (1/T - 1/T₀))
+    rA = -k * y[1] * y[2] * P^2
+    rB = rA
+    rC = -rA
 
-# 945.400 μs (3309 allocations: 168.98 KiB)
-# u: 2-element Vector{Float64}:
-#  0.9999634355313174
-#  0.9999315506115275
+    du[1] = rA
+    du[2] = rB
+    du[3] = rC
+end
 
-# Use Adaptive Differential Evolution method adaptive_de_rand_1_bin_radiuslimited from
-# BlackBoxOptim.jl, via OptimizationBBO. This method needs a bounded search area.
+# Set up the input values and pack into a tuple of parameters
+T = 510.0
+P = 10.0
+ΔP = 1.0
+p = (P, ΔP, T)
 
-prob = OptimizationProblem(rosenbrock, u0, p, lb = [-1.0, -1.0], ub = [1.0, 1.0])
-sol = solve(prob, BBO_adaptive_de_rand_1_bin_radiuslimited())
+# The starting values for the initial value problem
+u₀ = [60.0, 40.0, 0.0]
 
-# 13.260 ms (153718 allocations: 5.69 MiB)
-# u: 2-element Vector{Float64}:
-#  0.9999999999999495
-#  0.9999999999998918
+# The span over which to integrate - here the mass of catalyst
+tspan = (0.0, 20.0)
+
+# Create the ODEProblem object
+prob = ODEProblem(rates!, u₀, tspan, p)
+
+# Solve with the default solver
+sol = solve(prob)
+
+# Plot the result - the molar flows vs the mass of catalyst
+plot(sol, leg=:right)
+savefig("img/ODEmol.svg")
+
+# Now, we want to plot the molar fractions...
+# Copy over the independent values (sol.t) into our x array (optional)
+x = sol.t
+
+# For each entry in sol.u, sum up the molar flows - we get an array of values
+# with the total molar flow at every point in the reactor
+totflows = sum.(sol.u)
+
+# Now divide the individual flows with the total. 
+fracs = sol.u ./ totflows
+
+# We get the same structure as sol.u - an array of arrays.
+# We need to flatten this to plot it.
+y = hcat(fracs...)'
+plot(x,y, labels=["A" "B" "C"])
+savefig("img/ODEfrac.svg")
