13a8cc064b6161c895cc6e408b8b4f16a5a6c281
Braam van Dyk <braam.vandyk@gmail.com>
2023-05-14 21:58:09 +0200

    Added MTK section

--- scrathpad.jl
+++ scrathpad.jl
@@ -1,3 +1,31 @@
-using Symbolics
+using ModelingToolkit, DifferentialEquations, NonlinearSolve, Plots, Latexify
 
-@variables x, y
+@parameters σ ρ β
+@variables t x(t) y(t) z(t)
+D = Differential(t)
+
+eqs = [D(D(x)) ~ σ * (y - x),
+    D(y) ~ x * (ρ - z) - y,
+    D(z) ~ x * y - β * z]
+
+@named sys = ODESystem(eqs, t, [x, y, z], [σ, ρ, β])
+
+sys = structural_simplify(sys)
+
+equations(sys)
+
+u0 = [D(x) => 2.0,
+    x => 1.0,
+    y => 0.0,
+    z => 0.0]
+
+p = [σ => 28.0,
+    ρ => 10.0,
+    β => 8 / 3]
+
+tspan = (0.0, 100.0)
+prob = ODEProblem(sys, u0, tspan, p, jac = true)
+sol = solve(prob, Tsit5())
+
+plot(sol, idxs = (x, y))
+savefig("img/mtkexample2.svg")
