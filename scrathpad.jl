67fb0379d2695c382de1943916086dea71edbc78
Braam van Dyk <braam.vandyk@gmail.com>
2023-06-07 21:35:12 +0200

    DataFrames up to before joins

--- scrathpad.jl
+++ scrathpad.jl
@@ -2,7 +2,7 @@ using DataFrames
 
 df = DataFrame(X = 1:3:1500, Y = repeat(1:100, outer=5), Z = repeat(1:100, inner=5))
 
-filter(:Y => ==(50), df)
+
 
 
 select(df, [:X, :Z])
@@ -13,6 +13,8 @@ df.B = df.Y .+ df.Z
 df.C = df.Z .^ 2
 df
 
+describe(df)
+
 select(df, Cols(:A, Between(:X, :Z)))
 
 df[:, Cols(:A, Between(:X, :Z))]
@@ -21,3 +23,21 @@ df2 = select(df, Cols(:A, Between(:X, :Z)))
 df2
 
 select!(df, Cols(:A, Between(:X, :Z)))
+
+filter(:X => <=(10), df)
+df[df.X .<= 10, :]
+
+
+select(df, :X, :X => cumsum => :cumX)
+select(df, [:X, :Y], [:X, :Y] => ByRow((x, y) -> sin(x)*cos(y)) => :sinXcosY)
+select(df, AsTable(:) => ByRow(extrema) => [:min, :max])
+transform(df, AsTable(:) => ByRow(extrema) => [:min, :max])
+
+df = DataFrame(
+    Names = ["Tom", "Dick", "Harry"], 
+    AddressLine1 = ["2 Maple Drive", "4 Oak Street", "6 Pine Road"],
+    AddressLine2 = ["Hopetown", "Smallville", "Metropolis"],
+    AddressZip = [1234, 2345, 3456]
+)
+
+df[:, r"Addr"]
