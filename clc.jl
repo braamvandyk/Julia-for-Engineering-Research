cec58ac4ceb7f709308bf8cb7891b64445df62e2
braamvandyk <braam.vandyk@gmail.com>
2023-06-30 10:40:25 +0200

    CLC example start

---
+++ clc.jl
@@ -0,0 +1,15 @@
+#=
+# Chemical Looping Combusion in a fixed bed reactor with Cu/CH4
+
+**Model from:**
+
+Diglio G., Bareschino P., Mancusi E., Pepe F., 2017, Sensitivity analysis in the design of a packed-bed reactor for a
+chemical looping combustion process, Chemical Engineering Transactions, 57, 1027-1032 DOI: 10.3303/CET1757172
+
+
+## Reactions: 
+
+2Cu + O2 -> 2CuO (-312kJ/mol)
+4CuO + CH4 -> 
+=#
+
