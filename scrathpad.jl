5d3100c7aea5ba010f4615c93fc44b1da5cd727e
Braam van Dyk <braam.vandyk@gmail.com>
2023-06-30 15:35:14 +0200

    Add plotting back in

--- scrathpad.jl
+++ scrathpad.jl
@@ -1,11 +1,14 @@
-function addthem(a::T, b::S) where {T<:Number, S<:Number}
-    return a + b
-end
+using Plots
 
-function addthem(a::T, b::T) where T<:AbstractString
-    return a * b
-end
+f1(x, y) = x*sin(x) - y^2 * cos(y)
+f2(x, y) = x*cos(x) - y^2 * sin(y)
+x = range(0, 5, length=100)
+y = range(0, 3, length=50)
+z1 = @. f1(x', y)
+z2 = @. f2(x', y)
+contour(z1)
+contour!(z2)
 
-addthem(1, 2)
-addthem(2, 2.0)
-addthem("One", "Two")
+
+surface(x, y, z1)
+contour!(x, y, z1)
